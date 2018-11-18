765a2aaf50c9d81565d1e9852d629f9cff0e86c3
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-18 16:37:24 -0600

    Init files

---
+++ src/madvet.java
@@ -0,0 +1,14 @@
+// ------------------------------------------
+// madvet.java
+// Jeremy Campbell
+// Problem 26 in Applied Algorithms, practice
+// using a Breadth First Search
+// ------------------------------------------
+
+public class madvet {
+
+
+    public static void main (String[] args) {
+
+    }
+}
