5d1607f7f0027ce5b691faf461f904a083ad399e
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-18 17:20:58 -0600

    Init the Laboratory class. Still need to think through my approach.

--- src/madvet.java
+++ src/madvet.java
@@ -5,10 +5,54 @@
 // using a Breadth First Search
 // ------------------------------------------
 
+import java.io.IOException;
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class madvet {
 
+    private static class ConversionMachine {
+        private int first;
+        private int second;
+        private int third;
 
-    public static void main (String[] args) {
+        ConversionMachine(int first, int second, int third) {
+            this.first = first;
+            this.second = second;
+            this.third = third;
+        }
+    }
 
+    private static class Laboratory {
+        private int labNum;
+        private int numQuestions;
+        private List<ConversionMachine> machines = new ArrayList<>(3);
+
+        Laboratory(int labNum, int numQuestions) {
+            this.labNum = labNum;
+            this.numQuestions = numQuestions;
+        }
+
+        void initMachines(Scanner fin) {
+            for (ConversionMachine m : machines) {
+                // Read in the three animals that can be converted to
+                // in "forward" mode
+                m = new ConversionMachine(fin.nextInt(), fin.nextInt(), fin.nextInt());
+            }
+        }
+    }
+
+    public static void main (String[] args) throws IOException {
+        Scanner fin = new Scanner(new File("madvet.in"));
+        PrintWriter fout = new PrintWriter("madvet.out");
+
+        int numDataSets = fin.nextInt();
+        for (int i = 0; i < numDataSets; i++) {
+            Laboratory lab = new Laboratory(fin.nextInt(), fin.nextInt());
+
+        }
     }
 }
