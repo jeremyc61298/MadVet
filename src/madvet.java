d60a8af058c96e96f2837039fdd82922ea163f91
Jeremy Campbell <jeremyc61298@gmail.com>
2018-11-19 14:40:17 -0600

    Working on an idea for the conversions.

--- src/madvet.java
+++ src/madvet.java
@@ -9,39 +9,82 @@ import java.io.IOException;
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class madvet {
 
-    private static class ConversionMachine {
-        private int first;
-        private int second;
-        private int third;
+    private static int MAX_ANIMALS = 3;
 
-        ConversionMachine(int first, int second, int third) {
-            this.first = first;
-            this.second = second;
-            this.third = third;
+    private static class AnimalList {
+        // Animals are represented as numbers
+        public int[] animals;
+        public int numSteps;
+
+        AnimalList(int[] animals, int numSteps) {
+            this.animals = animals;
+            this.numSteps = numSteps;
         }
     }
 
     private static class Laboratory {
         private int labNum;
         private int numQuestions;
-        private List<ConversionMachine> machines = new ArrayList<>(3);
+        private int[] desiredAnimals = new int[MAX_ANIMALS];
+        private int[][] conversions = new int[3][];
 
         Laboratory(int labNum, int numQuestions) {
             this.labNum = labNum;
             this.numQuestions = numQuestions;
         }
 
-        void initMachines(Scanner fin) {
-            for (ConversionMachine m : machines) {
-                // Read in the three animals that can be converted to
-                // in "forward" mode
-                m = new ConversionMachine(fin.nextInt(), fin.nextInt(), fin.nextInt());
+        public void inputConversions(Scanner fin) {
+            conversions[0] = new int[]{fin.nextInt(), fin.nextInt(), fin.nextInt()};
+            conversions[1] = new int[]{fin.nextInt(), fin.nextInt(), fin.nextInt()};
+            conversions[2] = new int[]{fin.nextInt(), fin.nextInt(), fin.nextInt()};
+        }
+
+        // Forward conversions change one animal to one or more animals
+        // Takes an AnimalList and returns a new AnimalList
+
+        private int[] forward(int[] animals, int which) {
+            // If there is at least one "which" animal to convert from
+            int[] newAnimals = animals;
+            if (newAnimals[which] >= 1) {
+                newAnimals[which]--;
+                for (int i = 0; i < MAX_ANIMALS; i++) {
+                    newAnimals[i] += conversions[which][i];
+                }
             }
+            return newAnimals;
+        }
+
+        // Reverse conversions change one or more animals to one animal
+        // There will be three of them
+        private int[] reverse(int[] animals, int toWhich) {
+            int[] newAnimals = animals;
+
+            // If there is at least one of each animal to convert from
+            // in animals
+            boolean wasTrue = true;
+            for (int i = 0; i < MAX_ANIMALS; i++) {
+                if (animals[i] < conversions[toWhich][i]) {
+                    wasTrue = false;
+                }
+            }
+
+            // Convert
+            if (wasTrue) {
+                for (int i = 0; i < MAX_ANIMALS; i++) {
+                    newAnimals[i] -= conversions[toWhich][i];
+                }
+                newAnimals[toWhich]++;
+            }
+
+            return newAnimals;
         }
     }
 
@@ -52,7 +95,7 @@ public class madvet {
         int numDataSets = fin.nextInt();
         for (int i = 0; i < numDataSets; i++) {
             Laboratory lab = new Laboratory(fin.nextInt(), fin.nextInt());
-
+            lab.inputConversions(fin);
         }
     }
 }
